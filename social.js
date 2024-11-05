2421d1aa37e3b643e6bfbbe25410b76d6a43d778
EugeneAfriyie <groupeight00@gmail.com>
2024-11-05 03:10:12 +0000

    adding shadow when the messageicon is clicked

--- social.js
+++ social.js
@@ -1,7 +1,13 @@
 /// SIDEBAR
-
-
 const menuItems = document.querySelectorAll('.menu-item');
+
+//messages
+
+const messageNotification = document.querySelector('#messages-notifications');
+const messages = document.querySelector('.messages');
+const message = document.querySelector('.message');
+const messageSearch = document.querySelector('.message-search');
+
 
 
 //remove active classs from each menuitem
@@ -18,10 +24,24 @@ menuItems.forEach(item=>{
         changeActiveItem();
         item.classList.add('active');
 
-        if(item.id != 'notification'){
+        if(item.id != 'notifications'){
             document.querySelector('.notification-popup').style.display = 'none';
         }else{
             document.querySelector('.notification-popup').style.display = 'block';
+            document.querySelector('#notification .notification-count').style.display = 'none';
         }
     })
+})
+
+
+// ************** MESSAGES *******************
+
+messageNotification.addEventListener('click', ()=>{
+    messages.style.boxShadow = '0 0 1rem var(--color-primary)';
+    document.querySelector('#messages-notifications .notification-count').style.display = 'none';
+
+    setTimeout(()=>{
+        messages.style.boxShadow = 'none';
+
+    },2000)
 })
